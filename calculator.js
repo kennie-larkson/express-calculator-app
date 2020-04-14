4dbb5efac558fc4258de58c86eba540c8cc02d08
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-04-14 12:47:37 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,27 @@
+const express  = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+    
+})
+app.post('/',(req,res)=>{
+   
+    console.log(req.body)
+    const { firstNum, secondNum} = req.body
+    // console.log(Number(firstNum+secondNum))
+    let num_one = Number(firstNum)
+    let num_two = Number(secondNum)
+    let result = num_one + num_two
+    console.log(result)
+    res.send(`The result of your calculation is: ${result}`)
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server running on port: 3000")
+})
